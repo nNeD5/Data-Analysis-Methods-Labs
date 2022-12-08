c2602c15ac117814660017c0df0aec49ae4eed6c
oned <alexned2002@gmail.com>
2022-12-08 17:15:13 +0200

    t

--- HW_3/KMeansMethod/clusters_kmeans.py
+++ HW_3/KMeansMethod/clusters_kmeans.py
@@ -54,37 +54,5 @@ def main():
     save_clusters(row_number_and_average_exam_score)
 
 
-# def clusterization(data):
-#     k_means = KMeans()
-#     k_means.fit(data)
-#     k_means_cluster_centers = k_means.cluster_centers_
-#     print(k_means_cluster_centers)
-#     print(len(k_means_cluster_centers))
-
-
-# def plot_elbow_method(data):
-#     inertias = []
-#
-#     for i in range(1, len(data)):
-#         kmeans = KMeans(n_clusters=i)
-#         kmeans.fit(data)
-#         inertias.append(kmeans.inertia_)
-#     plt.plot(range(1, len(data)), inertias, marker='o')
-#     plt.title("Elbow method")
-#     plt.xlabel("Number of clusters")
-#     plt.ylabel("Inertia")
-#     plt.savefig("Elbow method")
-#
-#
-# def main():
-#     y = pd.read_csv("Data/minmax_normalized.csv")
-#     x = [i for i in range(0, len(y))]
-#     plot_elbow_method(y)
-#     # clusterization(y)
-#     # plt.scatter(x, y[score_labels[0]], c=["#1f77b4"])
-#     # plt.scatter(x, y[score_labels[1]], c=["#ff7f0e"])
-#     # plt.scatter(x, y[score_labels[2]], c=["#2ca02c"])
-
-
 if __name__ == "__main__":
     main()
