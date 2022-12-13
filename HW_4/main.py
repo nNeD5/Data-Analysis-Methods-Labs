f90737d2c76c5007b7d163247f68435efb7fde7f
alexned2002@gmail.com <alexned2002@gmail.com>
2022-12-13 14:27:06 +0200

    HW_4 done

--- HW_4/main.py
+++ HW_4/main.py
@@ -8,10 +8,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import random
-
 
 def main():
+    # Get data
     df = pd.read_csv('Data/cluster_data_0-2.csv')
     print(df)
     data = df[["math score", "reading score", "writing score"]]
@@ -20,29 +19,31 @@ def main():
     data_scaled = pd.DataFrame(scaler.fit_transform(data))
     pca_2 = PCA(n_components=2)
     x = pca_2.fit_transform(data_scaled)
-    print(typex)
+    
 
+    # SVM
     x_train, x_test, y_train, y_test = train_test_split(x, y)
-    clf = SVC(kernel='linear')
+    clf = SVC(kernel='poly', degree=3)
     clf.fit(x_train, y_train)
-    predictions = clf.predict(x_test)
-    print("Accuracy:", accuracy_score(predictions, y_test))
-
-    w = clf.coef_[0]
-    b = clf.intercept_[0]
-    x_visual = np.linspace(32,57)
-    y_visual = -(w[0] / w[1]) * x_visual - b / w[1]
+    y_predictions = clf.predict(x_test)
+    print("Accuracy:", accuracy_score(y_predictions, y_test))
+    print(clf.get_params())
 
     # Plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    # ax.scatter(x[:, 0], x[:, 1], c=y, edgecolors="k")
-    # ax.scatter(x=x_train[0], y=x_train[1], c=y, edgecolors="k")
-    ax.scatter(x[:, 0], x[:, 1], c=y, cmap=plt.cm.coolwarm)
-    ax.plot(x_visual, y_visual)
-    plt.savefig("/mnt/d/plt.png")
-
+    h = 0.02
+    x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+    y_min, y_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=plt.cm.coolwarm)
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.show()
 
 
 if __name__ == '__main__':
--- HW_4/test.py
+++ HW_4/test.py
@@ -1,36 +1,48 @@
-from seaborn import load_dataset, pairplot
-from seaborn import scatterplot
-
-import matplotlib.pyplot as plt
-
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
 
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features. We could
+                      # avoid this ugly slicing by using a two-dim dataset
+y = iris.target
+
+h = .02  # step size in the mesh
+C = 1.0  # SVM regularization parameter
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
+
+# create a mesh to plot in
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+# title for the plots
+titles = ['SVC with linear kernel',
+          'LinearSVC (linear kernel)',
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel']
 
 
-# Rerunning the algorithm with a binary classifier
-df = load_dataset('penguins')
-df = df.dropna()
-df = df[df['species'] != 'Gentoo']    # This limits us to two classes
+clf = poly_svc
+plt.subplot(1, 1, 1)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-# X = df.select_dtypes('number')
-X = df[['bill_length_mm', 'bill_depth_mm']]
-y = df['species']
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=100)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
 
-clf = SVC(kernel='linear')
-clf.fit(X_train, y_train)
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
+plt.title(titles[3])
 
-# Visualizing the linear function for our SVM classifier
-w = clf.coef_[0]
-b = clf.intercept_[0]
-x_visual = np.linspace(32,57)
-y_visual = -(w[0] / w[1]) * x_visual - b / w[1]
-
-scatterplot(data = X_train, x='bill_length_mm', y='bill_depth_mm', hue=y_train)
-plt.plot(x_visual, y_visual)
-plt.savefig("/mnt/d/plt.png")
+plt.show()
